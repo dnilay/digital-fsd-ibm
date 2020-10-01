e8f9623346496ff4da20a99469411d82fc2d7129
nilay <mailnilaydas@gmail.com>
2020-10-01 15:37:46 +0530

    created server using(req,res)=>)

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //http is a node core module
 const http = require('http');
-const server=http.createServer();
-server.on('connection',function (socket) {
-    console.log(socket);
-})
-//if we do not provide the port number by default the port is 8080
+const server=http.createServer(((req, res) => {
+    console.log(req.url);
+   console.log(req.headers);
+    console.log(req.method);
+
+    })
+
+);
+
 server.listen(3000);
-console.log('node server started on port 3000');
+console.log('server started on port 3000');
