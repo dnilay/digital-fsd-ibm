f356a4cba3b606a33af124dcdf4899d93fe0c5fa
nilay <mailnilaydas@gmail.com>
2020-10-05 17:59:17 +0530

    added array

--- application.js
+++ application.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: false}));
-app.get('/add-product', ((req, res, next) => {
-    res.send('<form action="/product" method="POST">' +
-        '<label>Provide Product Type:</label></label><input type="text" name="title"><br/>' +
-        '<input type="submit" value="addProduct"></form>')
+app.use(express.json());
+let generes = [
+    {id: 1, name: 'Romance',},
+    {id: 2, name: 'Action'},
+    {id:3,name:'Thriller'}
+    ]
+app.get('/api/generes', (req, res) => {
 
-}));
-app.post('/product', (req, res, next) => {
-console.log(req.body);
-res.redirect('/add-product');
-
-    }
-);
-app.use('/', (req, res, next)=>{
-    res.send('<h2>Welcome to my first app....</h2>');
+    res.send(generes);
 });
-const port=process.env.PORT || 3000;
-app.listen(3000,()=>{
-    console.log(`server started at http://localhost:${port}`);
-})
+/*
+=================
+will start tomorrow
+===================
+app.post('/api/generes',((req, res) => {
+    let genre={
+        id:generes.length+1,
+        name:req.body.name
+    }
+    generes.push(genre);
+    res.send(generes);
+}));
+*/
 
+app.use('/',((req, res) => {
+    console.log('hi this is error page');
+}))
+app.listen(3000);
