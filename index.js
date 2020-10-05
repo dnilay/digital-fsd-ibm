89810d5270cf860ce738f050957aecc83b834a1d
nilay <mailnilaydas@gmail.com>
2020-10-05 09:46:41 +0530

    routing added

---
+++ index.js
@@ -0,0 +1,29 @@
+const http=require('http');
+const customers=[
+    {id:1,name:'John'},
+    {id:2,name:'Marry'},
+    {id:3,name:'Jane'},
+]
+
+const server=http.createServer(function(req,res){
+
+    let url=req.url;
+    console.log(url);
+    if (req.url==="/")
+    {
+       res.write('<h1>home page for my app</h1>');
+       res.end();
+    }
+    else if (req.url==='/api/customers')
+    {
+        res.write(JSON.stringify(customers));
+        res.end();
+    }
+    else {
+        res.write('<h3>no page found</h3>');
+        res.end();
+    }
+
+});
+const port =  3000;
+server.listen(port, () => console.log(`Listening on port ${port}...`));
