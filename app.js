d0a1b29ac6609c4848b32b15b236759323caaae9
nilay <mailnilaydas@gmail.com>
2020-10-05 16:10:58 +0530

    added routing

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const http=require('http');
 const express = require('express');
 const app=express();
 
-app.use('/', (req, res, next) => {
+/*app.use('/', (req, res, next) => {
     console.log('Always Executes.!');
     next();
-});
+});*/
+app.use('/products',(req, res, next) => {
+    res.send('<h2>The Products Page</h2>');
+})
 app.use('/add-product', (req, res, next) => {
-    console.log('In  middleware!');
+    console.log('In add-product middleware!');
     res.send('<h2>The Add Product Page.</h2>');
 });
 app.use('/',function (req,res,next) {
-    console.log('In another middleware!');
+
     res.send('<h2>Hello Node JS.</h2>');
 
 });
