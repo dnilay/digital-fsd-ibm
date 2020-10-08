c2afe2895be5c49c80407fad78ccd9bcea757935
nilay <mailnilaydas@gmail.com>
2020-10-08 10:49:49 +0530

    save student object

--- index.js
+++ index.js
@@ -6,3 +6,37 @@ mongoose.connect('mongodb://localhost:27017/play-ground',{ useNewUrlParser: true
 .catch(reason => {
     console.log('Connection is not Unsuccessful..!',reason.message);
 })
+
+const studentSchema=new mongoose.Schema(
+    {
+        firstName:String,
+        lastName:String,
+        email:String,
+        doj:{type: Date,default:Date.now()}
+    });
+
+const Student=mongoose.model('Student',studentSchema);
+let student=new Student({
+    firstName: 'Marry',
+    lastName: 'Public',
+    email: 'marry@email.com'
+});
+async function createStudent()
+{
+    let result = await student.save();
+
+    console.log(result);
+}
+
+createStudent().then(function (){
+    console.log('Student created: ');
+
+}).catch(error => {
+    console.log('Error creating student:',error.message);
+});
+
+
+
+
+
+
