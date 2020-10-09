4d530b22906c1f15ce2dd1abf3a6666bda1a40f0
nilay <mailnilaydas@gmail.com>
2020-10-09 10:02:07 +0530

    added regex

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/play-ground', {useNewUrlParser: true, useUnifiedTopology: true});
-
-
+mongoose.connect('mongodb://localhost:27017/play-ground', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('connected successfully');
+});
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
-    tags: [ String ],
+    tags: [String],
     date: Date,
     isPublished: Boolean,
     price: Number
 });
-
 const Course = mongoose.model('Course', courseSchema);
-
 async function getCourses() {
     return Course
-        .find({isPublished: false})
-        .and([{tags: 'backend'}])
+        .find({isPublished: true})
+        .or([
+            {price: {$gte: 15}},
+            {name: /.*Course.*/i}
+        ])
         .sort('-price')
-        .select('name author price tags isPublished');
+        .select('name author price');
 }
-
 async function run() {
     const courses = await getCourses();
     console.log(courses);
 }
-
-run();
+run().then(() => {
+    console.log('success');
+});
