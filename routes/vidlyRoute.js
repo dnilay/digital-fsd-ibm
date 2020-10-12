930f49d740f7fc4c28cb55b538e113782cdb097d
nilay <mailnilaydas@gmail.com>
2020-10-12 09:23:44 +0530

    added findByIdAndUpdate

--- routes/vidlyRoute.js
+++ routes/vidlyRoute.js
@@ -9,17 +9,31 @@ const genreSchema = {
 const Genre = mongoose.model('genre', genreSchema);
 
 router.post('/', async (req, res) => {
-    const { error } = validateGenre(req.body);
+    const {error} = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    let genre = new Genre({ name: req.body.name });
+    let genre = new Genre({name: req.body.name});
     let result = await genre.save();
     res.send(result);
 });
-
-router.get('/',async (req, res) => {
+router.get('/', async (req, res) => {
     let result = await Genre.find();
     res.send(result);
 })
+
+router.put('/:id', async (req, res) => {
+    const {error} = validateGenre(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name},{new:true});
+    if ((!genre)) {
+        return res.status(404).send('The genre with the given ID was not found.');
+    }
+    res.send(genre);
+
+
+});
+
 function validateGenre(genre) {
     const schema = {
         name: Joi.string().min(5).required()
