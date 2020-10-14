ca18b21a0153cec3806ebcb32809077fbae5bc8b
nilay <mailnilaydas@gmail.com>
2020-10-14 14:51:47 +0530

    model finished

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,14 @@
-const products = [];
+const Product = require('../models/products');
 exports.getAddProduct=(req, res)=>
 {
     res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 }
 exports.postAddProduct = (req, res) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 exports.getProducts = (req, res) => {
+    const products = Product.fetchAll();
     res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 };
