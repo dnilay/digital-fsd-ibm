c0431336e2eb186c93ff66349deaf8e73566403b
nilay_das <nilaydas721@gmail.com>
2020-10-27 15:31:02 +0530

    node app

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Post = require("./models/post");
+const app = express();
+let posts=[];
+mongoose
+    .connect(
+        "mongodb://localhost:27017/play-ground", {useUnifiedTopology: true, useNewUrlParser: true}
+    )
+    .then(() => {
+        console.log("Connected to database!");
+    })
+    .catch((error) => {
+        console.log("Connection failed!", error);
+    });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+})
+app.get("/api/posts", (req, res, next) => {
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: "Posts fetched successfully!",
+            posts: documents
+        });
+    });
+});
+app.listen(3000);
