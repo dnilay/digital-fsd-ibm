6f3a442ad9690501144a225cc0b863833c54d73b
nilay_das <nilaydas721@gmail.com>
2020-10-28 15:12:08 +0530

    node backend-final

--- server.js
+++ server.js
@@ -49,12 +49,97 @@ app.post('/api/tutorials', (req, res) => {
       console.log('error');
  });
 });
-
 app.get('/api/tutorials',(req, res) => {
-  Tutorial.find(req.params.id).then((tutorial)=>{
-    res.send(tutorial);
-  }).catch(error=>{
-    console.log('error');
-  })
+    Tutorial.find(req.params.id).then((tutorial)=>{
+        res.send(tutorial);
+    }).catch(error=>{
+        console.log('error');
+    })
 })
+app.get('/api/tutorials/:id', (req, res) => {
+    const id = req.params.id;
+
+    Tutorial.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({ message: "Not found Tutorial with id " + id });
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving Tutorial with id=" + id });
+        });
+});
+app.get('/api/tutorials/published',(req, res) => {
+    Tutorial.find({ published: true })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
+})
+app.put('/api/tutorials/:id', (req,res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+
+    const id = req.params.id;
+
+    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            } else res.send({ message: "Tutorial was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Tutorial with id=" + id
+            });
+        });
+},);
+app.delete('/api/tutorials/:id', (req, res) => {
+    const id = req.params.id;
+
+    Tutorial.findByIdAndRemove(id, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            } else {
+                res.send({
+                    message: "Tutorial was deleted successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Tutorial with id=" + id
+            });
+        });
+});
+app.delete('/api/tutorials', (req, res) => {
+    Tutorial.deleteMany({})
+        .then(data => {
+            res.send({
+                message: `${data.deletedCount} Tutorials were deleted successfully!`
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while removing all tutorials."
+            });
+        });
+})
+
 app.listen(3000);
